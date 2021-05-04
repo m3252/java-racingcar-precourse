0d989d8b2f533c967628716d85b640a3121270a1
moon_ <m3252@naver.com>
2021-05-04 19:52:25 +0900

    test(RacingCars): 일급 컬렉션의 생성방식 테스트

--- src/test/java/race/RacingCarsTest.java
+++ src/test/java/race/RacingCarsTest.java
@@ -5,25 +5,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import race.entity.Distance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarsTest {
 
-    private RacingCar noah;
-    private RacingCar kim;
-    private RacingCar lee;
+    private final List<RacingCar> cars = new ArrayList<>();
 
     @BeforeEach
     public void setUp(){
-        noah = new RacingCar("noah");
-        kim = new RacingCar("kim");
-        lee = new RacingCar("lee");
+        cars.add(new RacingCar("noah"));
+        cars.add(new RacingCar("kim"));
+        cars.add(new RacingCar("lee"));
     }
 
     @Test
     @DisplayName("n 대의 자동차가 존재한다.")
     public void n_대의_자동차가_존재(){
-        RacingCars racers = new RacingCars(noah, kim, lee);
+        RacingCars racers = new RacingCars(cars);
         assertThat(racers.size()).isEqualTo(3);
     }
 }
