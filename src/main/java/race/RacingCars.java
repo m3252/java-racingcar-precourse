aae5adee924ebcd14dd360ed480e564556011dc9
moon_ <m3252@naver.com>
2021-05-04 19:52:07 +0900

    refactor(RacingCars): 일급 컬렉션의 생성방식 수정

--- src/main/java/race/RacingCars.java
+++ src/main/java/race/RacingCars.java
@@ -9,12 +9,19 @@ public class RacingCars {
 
     private final List<RacingCar> racingCars = new ArrayList<>();
 
-    public RacingCars(RacingCar ... car) {
-        this.racingCars.addAll(Arrays.asList(car));
+    public RacingCars(List<RacingCar> cars) {
+        checkNumberOfRacingCar(cars.size());
+        this.racingCars.addAll(cars);
     }
 
     public int size(){
         return racingCars.size();
+    }
+
+    private void checkNumberOfRacingCar(int size){
+        if( 2 > size || size > 100){
+            throw new IllegalArgumentException("참가 가능한 레이싱카는 2대 이상 100대 이하입니다.");
+        }
     }
 
     @Override
