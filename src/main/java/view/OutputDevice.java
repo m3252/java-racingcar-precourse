37d7d0fb1dd62c361711be455fadc870a060700d
moon_ <m3252@naver.com>
2021-05-04 22:59:32 +0900

    refactor: UI 출력 장치 리팩토링

--- src/main/java/view/OutputDevice.java
+++ src/main/java/view/OutputDevice.java
@@ -1,12 +1,19 @@
 package view;
 
+import jdk.internal.joptsimple.internal.Strings;
 import race.RacingCars;
+
+import java.util.List;
 
 public class OutputDevice {
 
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
-    private static final String WINNERS_MESSAGE = "%s 가 최종 우승했습니다.";
+    private static final String WINNERS_MESSAGE = "%s가 최종 우승했습니다.";
+
+    private static final String COLON = " : ";
+    private static final String HYPHEN = "-";
+
 
     public static void printInputCarNamesMessage() {
         System.out.println(INPUT_CAR_NAMES_MESSAGE);
@@ -17,14 +24,19 @@ public class OutputDevice {
     }
 
     public static void printExecutionResult(RacingCars racingCars) {
-
+        for (int i=0; i<racingCars.size(); i++) {
+            System.out.println(racingCars.get(i).getCarName()
+                    + COLON
+                    + new String(new char[racingCars.get(i).currentMoveDistance()]).replace("\0", HYPHEN));
+        }
+        System.out.println();
     }
 
-    public static void printWinners(String... carNames) {
+    public static void printWinners(List<String> winnerNames) {
         StringBuilder winners = new StringBuilder();
-        for (String carName : carNames) {
-            winners.append(carName);
+        for (String carName : winnerNames) {
+            winners.append(carName).append(",");
         }
-        System.out.printf(WINNERS_MESSAGE, winners.toString());
+        System.out.printf(WINNERS_MESSAGE, winners.substring(0, winners.length()-1));
     }
 }
